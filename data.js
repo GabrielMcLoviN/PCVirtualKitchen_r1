284efe3fc4196c29ace920cc6eb894f238659d9e
will greer <willfromspace@gmail.com>
2021-01-12 13:20:18 -0700

    init

---
+++ data.js
@@ -0,0 +1,189 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen_01",
+      "name": "Kitchen_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.28366228185752895,
+        "pitch": 0.2409571701441493,
+        "fov": 1.2739094032009168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.4252217215869756,
+          "pitch": 0.534560200015882,
+          "title": "nonstick skillet",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.18578207954119463,
+          "pitch": 0.44039175033335454,
+          "title": "dutch oven",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.5233019513478805,
+          "pitch": 0.5346585714994525,
+          "title": "cast iron",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7828439501751578,
+          "pitch": 0.3000919208252064,
+          "title": "air fryer",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.724540658980688,
+          "pitch": 0.5924236607609039,
+          "title": "pizza peel",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.20558290427587,
+          "pitch": 0.41308350229582835,
+          "title": "salad bowl",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.824589139508083,
+          "pitch": 0.5197358315170817,
+          "title": "charcuterie",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.0210249388923707,
+          "pitch": 0.6610016937252077,
+          "title": "manual food processor",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.4655333888327764,
+          "pitch": 0.5663247914573244,
+          "title": "small square",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.9926161406403882,
+          "pitch": 0.24708123080548816,
+          "title": "insulated serving bowl",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-kitchen_02",
+      "name": "Kitchen_02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.15055228892836148,
+        "pitch": 0.5206078285644296,
+        "fov": 1.3848081938437478
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.2741263168624304,
+          "pitch": 0.5519410325766607,
+          "title": "pizza peel",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.4724006416181243,
+          "pitch": 0.650736876317616,
+          "title": "charcuterie",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.8870758070501026,
+          "pitch": 0.5350841529736705,
+          "title": "food processor",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.2521911697484196,
+          "pitch": 0.5683008435471066,
+          "title": "nonstick",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.5386061207202886,
+          "pitch": 0.4536125956700072,
+          "title": "dutch oven",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.068091802238964,
+          "pitch": 0.4347360382781833,
+          "title": "cool n serve",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.6045386506152992,
+          "pitch": 0.2434178822133557,
+          "title": "airfryer",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.3545964848335714,
+          "pitch": 0.4513371953140872,
+          "title": "salad bowl",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.093563192650258,
+          "pitch": 0.5994963555682666,
+          "title": "cast iron",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "pcpano1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
