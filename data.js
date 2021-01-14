01749eec30acc9c92322ff5d437b875bb09985af
will greer <willfromspace@gmail.com>
2021-01-14 13:15:32 -0700

    content

--- data.js
+++ data.js
@@ -62,7 +62,7 @@ var APP_DATA = {
 					pitch: 0.3000919208252064,
 					title: "DELUXE AIR FRYER",
 					images: "https://placehold.it/500x500?text=placeholder",
-
+					
 					text:
 						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
 					subhead: "Subheading",
