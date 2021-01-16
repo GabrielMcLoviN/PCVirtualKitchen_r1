73122d5496ecddf9baf5d96883469dbfc244af51
will greer <willfromspace@gmail.com>
2021-01-15 18:45:30 -0700

    carousel fully functional

--- data.js
+++ data.js
@@ -64,7 +64,11 @@ var APP_DATA = {
 					yaw: 0.5233019513478805,
 					pitch: 0.5346585714994525,
 					title: '12" CAST IRON SKILLET',
-					images: ["https://placehold.it/500x500?text=placeholder"],
+					images: [
+						"https://placehold.it/500x500?text=placeholder",
+						"https://placehold.it/500x500?text=placeholder",
+						"https://placehold.it/500x500?text=placeholder",
+					],
 					text:
 						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
 					subhead: "Subheading",
