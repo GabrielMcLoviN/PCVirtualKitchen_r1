05a166ff47d89f38ce73677aac6583fcf0718347
will greer <willfromspace@gmail.com>
2021-01-17 18:02:10 -0700

    add data copy per ppt

--- data.js
+++ data.js
@@ -1,5 +1,3 @@
-
-
 var APP_DATA = {
 	scenes: [
 		{
@@ -37,57 +35,140 @@ var APP_DATA = {
 					pitch: 0.534560200015882,
 					title: '12" STAINLESS STEEL NONSTICK SKILLET',
 					images: [
-						"./products/skillet/skillet.jpg",
-						"./products/skillet/skillet2.jpg",
-						"./products/skillet/skillet3.jpg",
-						"./products/skillet/skillet4.jpg",
+						"./products/nonstick_01.jpg",
+						"./products/nonstick_02.jpg",
+						"./products/nonstick_03.jpg",
+						"./products/nonstick_04-@prettyfocusedcourse.jpg",
+						"./products/nonstick_05-@jessywhitenton.jpg",
+						"./products/nonstick_06-@whatslindacooking.jpg",
 					],
+					subhead: '12" Stainless Steel Nonstick Skillet',
 					text:
-						"This innovative pan combines the best features of stainless steel and nonstick cookware. The nonstick mesh coating provides the perfect sear (without the mess) and leaves fond for pan sauces and gravy. The size is perfect for one-pot meals.",
-					subhead: "GO-TO SKILLET FOR ONE-POT MEALS",
+						"<p>This innovative pan combines the best features of stainless steel and nonstick cookware. The nonstick mesh coating provides the perfect sear (without the mess) and leaves fond for pan sauces and gravy. The size is perfect for one-pot meals.</p><br><p>Our stainless steel skilletsare a true hybrid: They combine the amazing cooking and heating capabilities of traditional stainless steel with the easy cleanup of nonstick cookware. With these innovative pans, you can get the perfect sear, make professional-grade pan sauces, and look like a pro—without breaking a sweat.</p>",
+					recipes: [
+						{
+							id: "Chicken Sausage & Spinach Ravioli",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+						{
+							id: "Egg Roll in a Bowl",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+						{
+							id: "Roasted Chicken & Asparagus",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+					],
 				},
 				{
 					yaw: 0.18578207954119463,
 					pitch: 0.44039175033335454,
 					title: "6-QT. ENAMELED DUTCH OVEN",
 					images: [
-						"./products/cast-iron.jpg",
-						"./products/dutchoven.jpg",
-						"./products/dutchoven.jpg",
-						"./products/dutchoven.jpg",
+						"./products/dutchoven_01.jpg",
+						"./products/dutchoven_02.jpg",
+						"./products/dutchoven_03-@muffinsandmugs.jpg",
+						"./products/dutchoven_04.jpg",
+						"./products/dutchoven_05.jpg",
+						"./products/dutchoven_06.jpg",
 					],
+					subhead: "6-qt. Enameled Dutch Oven",
 					text:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-					subhead: "Subheading",
+						"<p>Enameled cast iron is the kind of cookware that will become a fixture at every special meal you make—and that includes weeknight meals. This Dutch oven is beautiful so it’s ready to go directly from your stove (including induction) or oven to your table. You’ll reach for it again and again because it’s low maintenance and versatile.</p><br><p>The 6-qt. (5.7-L) Dutch oven has a light-colored enameled interior, which means you can start using it right away—no seasoning required—and it lets you easily see food browning so you can avoid scorching it. Cast iron’s excellent heat retention means that food stays hot and delicious even if not everyone makes it to the table as soon as dinner’s ready.</p><br><p>The inside of the lid is covered with bumps that redirect the condensation in the pot to drip back onto the food, making it a self-basting pot. This makes your food juicy, tender, and more flavorful.</p>",
+					recipes: [
+						{
+							id: "01",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+						{
+							id: "02",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+						{
+							id: "03",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+					],
 				},
 				{
 					yaw: 0.5233019513478805,
 					pitch: 0.5346585714994525,
 					title: '12" CAST IRON SKILLET',
 					images: [
-						"https://placehold.it/500x500?text=placeholder",
-						"https://placehold.it/500x500?text=placeholder",
-						"https://placehold.it/500x500?text=placeholder",
+						"./products/castiron_01.jpg",
+						"./products/castiron_02.jpg",
+						"./products/castiron_03.jpg",
+						"./products/castiron_04-@makeandheart.jpg",
 					],
+					subhead: '12" Cast Iron Skillet',
 					text:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-					subhead: "Subheading",
+						'<p>Our 12" skillet comes preseasoned, so you can start using it right away on most heat sources. And its natural, nonstick surface gets better and better the more you use it.</p><br><p>Reach for this pan when you need a perfect sear on steaks or a golden, crispy crust on baked treats. Our skillet has a one-of-a-kind shape that gives you more cooking area—enough to fit four chicken breasts at one time. No need to cook in batches to feed your friends or family.</p><br><p>The two, low-profile pour spouts let you easily drain off pan juices whether you’re left-or right-handed. With the cooking traditions and durability of cast iron comes a certain weightiness, but not to worry, our skillet has two handles to help you lift and carry it.</p>',
+					recipes: [
+						{
+							id: "Stovetop Mac & Cheese",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "./recipes/post-recipe-homemade-pizza-dough-steps.pdf",
+						},
+						{
+							id: "Chili Lime Pork",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "./recipes/post-recipe-homemade-pizza-dough-steps.pdf",
+						},
+						{
+							id: "Blackened Chicken",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "./recipes/post-recipe-homemade-pizza-dough-steps.pdf",
+						},
+					],
 				},
 				{
 					yaw: 0.7828439501751578,
 					pitch: 0.3000919208252064,
 					title: "DELUXE AIR FRYER",
-					images: ["https://placehold.it/500x500?text=placeholder"],
-
+					images: [
+						"./products/fryer_01.jpg",
+						"./products/fryer_02-@jennlott.jpg",
+						"./products/fryer_03-@lazynolaketo.jpg",
+						"./products/fryer_04.jpg",
+						"./products/fryer_05.jpg",
+						"./products/fryer_06.jpg",
+					],
+					subhead: "Deluxe Air Fryer",
 					text:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-					subhead: "Subheading",
+						"<h4>AIR FRYER, ROTISSERIE, & DEHYDRATOR ALL-IN-ONE</h4><br><p>Enjoy fried foods at home without all the mess, hassle, and oil of deep frying but with all the flavor and texture you love. Ours is family-sized to quickly cook up to six servings, a whole chicken or roast, or appetizers for a crowd. Like a convection oven, superheated air circulates giving food a crispy texture. No preheating is necessary, saving you precious time. Conveniently fits under kitchen cabinets.</p><br><p>You can make perfectly cooked rotisserie chicken, roast veggies, bake dessert, make beef jerky, and dehydrate fruits and veggies for healthy snacks. Let’s get cooking.</p>",
+					recipes: [
+						{
+							id: "Homemade Beef Jerky",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "./recipes/post-recipe-homemade-pizza-dough-steps.pdf",
+						},
+						{
+							id: "Kebab Combinations",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "./recipes/post-recipe-homemade-pizza-dough-steps.pdf",
+						},
+						{
+							id: "Potato Tacos",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "./recipes/post-recipe-homemade-pizza-dough-steps.pdf",
+						},
+					],
 				},
 				{
 					yaw: -2.724540658980688,
 					pitch: 0.5924236607609039,
 					title: "PIZZA PEEL",
-					images: ["./products/pizzapeel_01.jpg"],
+					images: [
+						"./products/pizzapeel_01.jpg",
+						"./products/pizzapeel_02-@tictacseaux_in_the_kitchen.jpg",
+						"./products/pizzapeel_03-@MidwestLifeandStyle.jpg",
+					],
 					subtitle: "PIZZA PEEL",
 					subhead: "Pizza Peel",
 					text:
@@ -109,41 +190,109 @@ var APP_DATA = {
 					yaw: 2.20558290427587,
 					pitch: 0.41308350229582835,
 					title: "WOOD SALAD BOWL & SERVERS SET",
-					images: ["https://placehold.it/500x500?text=placeholder"],
-
+					images: [
+						"./products/salad_01.jpg",
+						"./products/salad_02.jpg",
+						"./products/salad_03-@thecraftofcooking.jpg",
+						"./products/salad_04-@theparsleypotato.jpg",
+					],
+					subhead: "Wood Salad Bowl & Servers Set",
 					text:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-					subhead: "Subheading",
+						"<p>Even the simplest salads look impressive when served with this set. Our Wood Salad Bowl and Servers Set is made out of acacia wood with a rich color and beautiful grain that is not only durable, but moisture resistant. Each piece is one of a kind because of the natural variations in the wood.</p><br>The salad bowl holds a standard bag of lettuce with room for extra salad fixings. The serving spoon and fork are about 12-inches long and fit naturally into any party or dinner setting.<p>",
+					recipes: [
+						{
+							id: "Classic Cobb Salad",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+						{
+							id: "Superfood Salad",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+						{
+							id: "Southwestern Salad",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+					],
 				},
 				{
 					yaw: 2.824589139508083,
 					pitch: 0.5197358315170817,
 					title: "PREMIUM CHARCUTERIE & CHEESE BOARD SET",
-					images: ["https://placehold.it/500x500?text=placeholder"],
-
+					images: [
+						"./products/charc_01.jpg",
+						"./products/charc_02.jpg",
+						"./products/charc_03.jpg",
+					],
+					subhead: "Premium Charcuterie & Cheese Board Set",
 					text:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-					subhead: "Subheading",
+						"<p>The Premium Charcuterie & Cheese Board Set has everything you need to make entertaining a breeze. The set features an Acacia wood Lazy Susan and our Charcuterie & Cheese Board Accessories.</p>",
+					related_content: [
+						{
+							id: "How to Build a Charcuterie Board",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+					],
 				},
 				{
 					yaw: 3.0210249388923707,
 					pitch: 0.6610016937252077,
 					title: "MANUAL FOOD PROCESSOR",
-					images: ["https://placehold.it/500x500?text=placeholder"],
-
+					images: [
+						"./products/foodprocessor_01.jpg",
+						"./products/foodprocessor_02.jpg",
+						"./products/foodprocessor_03-@erica-ekman.jpg",
+					],
+					subhead: "Manual Food Processor",
 					text:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-					subhead: "Subheading",
+						"<p>Our Manual Food Processor can take care of nearly all your chopping and mincing needs, without needing an outlet. Just add your ingredients to the bowl and pump the handle for fast salsa, dip, cookie dough, baby food, tapenade. The more you pump, the finer it cuts! And when you’re done, the handle locks into the lid for easy storage in your cabinets.</p>",
+					recipes: [
+						{
+							id: "Pico De Gallo",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+						{
+							id: "Cucumber-Dill Guacamole",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+						{
+							id: "Cranberry Salsa",
+							preview_image: "./recipes/recipe_imageLinks/pizza-dough.jpg",
+							link: "",
+						},
+					],
+				},
+				{
+					yaw: 2.4655333888327764,
+					pitch: 0.5663247914573244,
+					title: "SMALL SQUARE COOL & SERVE",
+					images: [
+						"./products/serve_01.jpg",
+						"./products/serve_02.jpg",
+						"./products/serve_03-@_laura_prietto_.jpg",
+					],
+					subhead: "Small Square Cool & Serve",
+					text:
+						"<p>The fun-sized Small Square Cool & Serve is perfect for parties, picnics, snacks, lunch on the go, and road trips. The exclusive gel insert (which features a handy finger slot for easy removal) keeps veggies, fruit, cheese, deli meat, dip, and other snacks cool for 4–6 hours—even outdoors!</p><br><p>It comes with a clear insert divided into two compartments with a 1-cup dip well in the center and has solid walls that keep food from mixing. The lid snaps onto the base so it’s easy to transport, and it even fits neatly underneath while in use.</p>",
 				},
 				{
 					yaw: 2.4655333888327764,
 					pitch: 0.5663247914573244,
 					title: "INSULATED SERVING BOWL SET",
-					images: ["https://placehold.it/500x500?text=placeholder"],
-
+					images: [
+						"./products/insulated_01.jpg",
+						"./products/insulated_02.jpg",
+						"./products/insulated_03.jpg",
+						"./products/insulated_04-@lazynolaketo.jpg",
+					],
+					subhead: "Insulated Serving Bowl Set",
 					text:
-						"The fun-sized Small Square Cool & Serve is perfect for parties, picnics, snacks, lunch on the go, and road trips. The exclusive gel insert (which features a handy finger slot for easy removal) keeps veggies, fruit, cheese, deli meat, dip, and other snacks cool for 4–6 hours—even outdoors! It comes with a clear insert divided into two compartments with a 1-cup dip well in the center, and has solid walls that keep food from mixing. The lid snaps onto the base so it’s easy to transport, and it even fits neatly underneath while in use.",
-					subhead: "Subheading",
+						"<p>For holidays, potlucks, or any big meal, it can feel nearly impossible to keep everything warm. With this insulated serving bowl set, you can keep foods warm up to 2 hours.</p><br><p>The bowls’ double-walled vacuum insulation keeps food hot, prevents sweating, and has a stay-cool exterior. Works with cold foods, too!</p><br><p>Each bowl comes with a clear plastic lid that’s double-walled and lets you see what's inside. The lid also hooks onto the side of the bowl, making it easier to dish out and serve food.</p>",
 				},
 			],
 		},
