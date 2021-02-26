467f88bf4a0615986487bf59f2a9fe590580e126
will greer <willfromspace@gmail.com>
2021-02-25 20:43:33 -0700

    rmv iframe

--- dist/app-files.e31bb0bc.js
+++ dist/app-files.e31bb0bc.js
@@ -24128,7 +24128,7 @@ var APP_DATA = {
       images: [''],
       subhead: '',
       fullWidth: true,
-      text: "<iframe allowfullscreen='true' style='border:none;width:100%;height:100%;' src='https://e.issuu.com/embed.html?d=p30530-012021usca-eat-well-ebook&hideIssuuLogo=true&hideShareButton=true&u=pamperedchef'></iframe>"
+      text: ""
     }, {
       yaw: -0.7416328666204635,
       pitch: 0.3615805204161795,
